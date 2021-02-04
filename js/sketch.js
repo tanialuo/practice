a599b003582dfbcc10bc60b5767c172fdedcc336
TaniaLuo <hluo@saic.edu>
2021-02-04 01:07:47 -0600

    new commit

---
+++ js/sketch.js
@@ -0,0 +1,87 @@
+
+let squareX = 750;
+let squareY = 750;
+let r = 30;
+let fr = 8;
+let rectLeftW = 500;
+let rectLeftH = 500;
+let rectRightW = 500;
+let rectRightH = 500;
+let textUp = 'are you tired of being RAMDOM?'
+let textDown = 'me neither'
+let leading = 50;
+
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  frameRate(fr);
+
+  rectMode(CENTER);
+
+
+}
+
+
+function draw() {
+  // put drawing code here
+  background(20);
+
+
+  //create line square
+
+  for(i = windowWidth-rectRightW ; i<= windowWidth ; i+=20){
+
+
+    strokeWeight(random(5,10));
+    stroke(random(25,75));
+    line(i, windowHeight-rectRightH, i, windowHeight);
+
+  }
+
+
+  //create a centric circle
+
+  for(i = 0 ; i<= rectLeftW ; i+=50){
+
+      noFill();
+      strokeWeight(random(5,10));
+      stroke(random(25,75));
+      rect(rectLeftW/2,rectLeftH/2,rectLeftW-i,rectLeftH-i);
+
+    }
+
+
+  //create matrix out of circles
+
+  for(i = windowHeight/2-squareY/2 ; i <= windowHeight/2+squareY/2 ; i+=r+10){
+
+  for(j = windowWidth/2-squareX/2 ; j <= windowWidth/2+squareX/2; j+=r+10){
+
+    noStroke();
+    fill(random(255),random(255),random(255));
+    circle(j,i,random(r));
+
+  }
+
+  }
+
+
+  //text messages
+  fill(255);
+  textSize(24);
+
+  textAlign(RIGHT);
+  text(textUp, windowWidth/2+squareX/2, windowHeight/2-squareY/2-leading);
+
+  textAlign(LEFT);
+  text(textDown, windowWidth/2-squareX/2, windowHeight/2+squareY/2+leading);
+
+
+
+ //the mouse
+ fill(255);
+ quad(mouseX+r/2, mouseY, mouseX, mouseY+r/2, mouseX-r/2, mouseY, mouseX, mouseY-r/2);
+
+
+}
